1fccfb1a2283c471faba4ab66d0235ce64f7389c
david william da costa <davidddwlliam@gmail.com>
2021-07-04 10:17:07 +0700

    migration

---
+++ database/migrations/2021_07_04_030940_create_details_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDetailsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('details', function (Blueprint $table) {
+            $table->id();
+            $table->string('nama_detail_product');
+            $table->text('fasilitas');
+            $table->string('type');
+            $table->integer('harga');
+            $table->string('image_url');
+            $table->integer('quantity');
+            $table->integer('discount');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('details');
+    }
+}
