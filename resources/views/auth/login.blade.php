79c5a7a21d7c4a056f6b121053077c88b1dbbbfd
izanagiwarrior <m.faiztpaduhai@gmail.com>
2021-07-04 23:51:38 +0700

    kurang login regitser gk masuk

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -71,7 +71,7 @@
                                 <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
                             </div>
                             <button type="submit" class="btn-login">Login</button>
-                            <a href="" class="d-flex justify-content-end mt-4">Forgot Password?</a>
+                            <a href="{{ route('password.request') }}" class="d-flex justify-content-end mt-4">Forgot Password?</a>
                             </method=>
                     </div>
                 </div>
